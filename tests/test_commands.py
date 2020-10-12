0671f1aa0e4ab50719ceb5c946d611b122da4221
Capi Etheriel <barraponto@gmail.com>
2020-10-12 15:56:22 -0300

    test create-user command

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,5 +1,6 @@
 """Test custom flask commands."""
 import pytest
+from selfsolver.commands.create_user import create_user
 from selfsolver.commands.database import create_all, recreate_all
 from selfsolver.commands.secret import generate_secret
 
@@ -28,6 +29,31 @@ def db_drop_all(monkeypatch, mocker):
     return stub
 
 
+@pytest.fixture()
+def db_session_add(monkeypatch, mocker):
+    """Monkeypatch db.session.add and provide stub to check calls."""
+    stub = mocker.stub(name="db.session.add")
+    monkeypatch.setattr("selfsolver.models.db.session.add", stub)
+    return stub
+
+
+@pytest.fixture()
+def db_session_commit(monkeypatch, mocker):
+    """Monkeypatch db.session.commit and provide stub to check calls."""
+    stub = mocker.stub(name="db.session.commit")
+    monkeypatch.setattr("selfsolver.models.db.session.commit", stub)
+    return stub
+
+
+@pytest.fixture()
+def mock_user(monkeypatch, mocker):
+    """Provide MockUser class and stub."""
+    mock_user = mocker.stub(name="MockUser")
+    monkeypatch.setattr("selfsolver.commands.create_user.User", mock_user)
+
+    return mock_user
+
+
 def test_generate_secret(runner):
     """Test generate-secret command."""
     result = runner.invoke(generate_secret)
@@ -48,4 +74,16 @@ def test_database_reset(runner, db_create_all, db_drop_all):
     result = runner.invoke(recreate_all)
     db_create_all.assert_called_once()
     db_drop_all.assert_called_once()
+    assert result.exit_code == 0
+
+
+def test_create_user(runner, db_session_add, db_session_commit, mock_user):
+    """Test create-user command."""
+    result = runner.invoke(create_user, ["email@nanana.com", "tijolo22"])
+    mock_user.assert_called_once_with(  # noqa: S106
+        email="email@nanana.com",
+        password="tijolo22",
+    )
+    db_session_add.assert_called_once_with(mock_user.return_value)
+    db_session_commit.assert_called_once()
     assert result.exit_code == 0
