e6c24bb2dff632eca7e118aa05d88227d7d3e488
Capi Etheriel <barraponto@gmail.com>
2020-10-13 12:44:32 -0300

    use schemas to validate input data

--- selfsolver/blueprints/auth.py
+++ selfsolver/blueprints/auth.py
@@ -1,10 +1,18 @@
 """Provide routes for user authentication."""
 from flask import abort, Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token
+from marshmallow import fields, Schema, ValidationError
 from selfsolver.models import User
 from selfsolver.password import verify
 
 auth = Blueprint("auth", __name__)
+
+
+class LoginSchema(Schema):
+    """Schema for login request data."""
+
+    email = fields.Email(required=True)
+    password = fields.String(required=True)
 
 
 @auth.route("/login", methods=["POST"])
@@ -13,8 +21,12 @@ def login():
     if not request.is_json:
         abort(400)
 
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
+    try:
+        data = LoginSchema().load(request.json)
+    except ValidationError:
+        abort(400)
+
+    email, password = data["email"], data["password"]
 
     if not email or not password:
         abort(400)
