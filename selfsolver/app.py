ca7d26bcf3deb6d6eea8b8ecca99c8622c6ffd54
Capi Etheriel <barraponto@gmail.com>
2020-10-23 21:27:44 -0300

    fix: register company create command

--- selfsolver/app.py
+++ selfsolver/app.py
@@ -7,6 +7,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from selfsolver.blueprints.auth import auth
+from selfsolver.commands.company import company_cli
 from selfsolver.commands.database import database_cli
 from selfsolver.commands.secret import generate_secret
 from selfsolver.commands.user import user_cli
@@ -21,6 +22,7 @@ CORS(app, origins=[app.config["SELFSOLVER_ENDUSER_APP"]])
 
 app.register_blueprint(auth)
 
-app.cli.add_command(database_cli)
 app.cli.add_command(generate_secret)
+app.cli.add_command(database_cli)
+app.cli.add_command(company_cli)
 app.cli.add_command(user_cli)
