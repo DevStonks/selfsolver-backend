5563a349d6a23e70bd5781e195f3d70ad5722a85
Capi Etheriel <barraponto@gmail.com>
2020-10-27 20:06:26 -0300

    test missing parameters for Location model

--- tests/models/test_location_model.py
+++ tests/models/test_location_model.py
@@ -1,5 +1,6 @@
 """Test selfsolver location model."""
 import pytest
+from sqlalchemy.exc import IntegrityError
 
 from selfsolver.models import Location
 
@@ -18,3 +19,12 @@ def test_location_creation(db_session, company, label):
 
     assert location.id
     assert location.label == label
+
+
+@pytest.mark.parametrize("option", ["label"])
+def test_location_creation_without_required(db_session, location_factory, option):
+    """Test location creation fails without required parameters."""
+    location = location_factory.build(**{option: None})
+    db_session.add(location)
+    with pytest.raises(IntegrityError, match="psycopg2.errors.NotNullViolation"):
+        db_session.commit()
