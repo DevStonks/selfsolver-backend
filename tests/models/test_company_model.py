ab66b030196ef00081f47005df90cabe46a91263
Capi Etheriel <barraponto@gmail.com>
2020-10-27 17:37:39 -0300

    cover cascades in company delete test

--- tests/models/test_company_model.py
+++ tests/models/test_company_model.py
@@ -1,5 +1,16 @@
 """Test selfsolver company model."""
-from selfsolver.models import Company, User
+import pytest
+
+from selfsolver.models import Company, Location, User
+
+
+@pytest.fixture()
+def user_and_location(db_session, user, location_factory):
+    """Provide both user and location fixtures with the same company."""
+    location = location_factory(company=user.company)
+    db_session.add(location)
+    db_session.commit()
+    return user, location
 
 
 def test_company_creation(db_session):
@@ -11,9 +22,11 @@ def test_company_creation(db_session):
     assert company.id
 
 
-def test_company_cascades(db_session, user):
-    """Test users are deleted when company is deleted."""
+def test_company_cascades(db_session, user_and_location):
+    """Test users and locations are deleted when company is deleted."""
+    user, location = user_and_location
     db_session.delete(user.company)
     db_session.commit()
 
     assert not db_session.query(User).all()
+    assert not db_session.query(Location).all()
