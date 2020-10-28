1ed9f05dd3b2f6b280c950550d204b9cb70d5cd6
Capi Etheriel <barraponto@gmail.com>
2020-10-28 10:51:11 -0300

    simplify company cascade test

--- tests/models/test_company_model.py
+++ tests/models/test_company_model.py
@@ -4,15 +4,6 @@ import pytest
 from selfsolver.models import Company, Location, User
 
 
-@pytest.fixture()
-def user_and_location(db_session, user, location_factory):
-    """Provide both user and location fixtures with the same company."""
-    location = location_factory(company=user.company)
-    db_session.add(location)
-    db_session.commit()
-    return user, location
-
-
 def test_company_creation(db_session):
     """Test company creation."""
     company = Company()
@@ -22,9 +13,9 @@ def test_company_creation(db_session):
     assert company.id
 
 
-def test_company_cascades(db_session, user_and_location):
+@pytest.mark.usefixtures("db_session", "location")
+def test_company_cascades(db_session, user):
     """Test users and locations are deleted when company is deleted."""
-    user, location = user_and_location
     db_session.delete(user.company)
     db_session.commit()
 
